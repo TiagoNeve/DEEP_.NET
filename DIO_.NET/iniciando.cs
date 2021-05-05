0808b617101ffa6229c7806b233d80581e76c96f
Tiago Neves <rykarumstuler@gmail.com>
2021-05-05 20:29:41 -0300

    Fim da aula Iniciando com dotnet

--- DIO_.NET/iniciando.cs
+++ DIO_.NET/iniciando.cs
@@ -1,3 +1,8 @@
 // Iniciando em C#, tudo vai dar certo
 
 // Iniciando com .NET
+/*
+    Para verificar a versão do dotnet basta executar o código: 
+    dotnet --help -> Mostra todos os comandos que o dotnet pode executar via linha de comando
+    dotnet [command] --help -> Mostra o help do comando em questão 
+*/
