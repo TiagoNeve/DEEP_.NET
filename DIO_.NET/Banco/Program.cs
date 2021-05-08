60bbf8e6ab7ee7384b1a8efe65d9327e4ca214ec
Tiago Neves <rykarumstuler@gmail.com>
2021-05-08 15:49:24 -0300

    Fim da parte 4

--- DIO_.NET/Banco/Program.cs
+++ DIO_.NET/Banco/Program.cs
@@ -1,16 +1,109 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Banco
 {
     class Program
     {
+        // Criação das listas de contas na memória:
+        static List<Conta> listaContas = new List<Conta>();
+
         static void Main(string[] args)
         {
-            Conta contaUsuario = new Conta(TipoConta.PessoaFisica, 1000, 3000, "Stuler");
-            // Criando um novo objeto chamado minhaConta da classe Conta, logo irá herdar os métodos e atributos.
-            // Dessa forma é settar um valor do atributo minhaConta sem estragar a classe, pois o valor só será modificado
-            // no objeto.
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            Console.WriteLine(contaUsuario.ToString());
+            while (opcaoUsuario != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                        //Depositar();
+                        break;
+                    case "C":
+                        Console.Clear(); // Limpa terminal
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listagem de contas Ativas");
+            Console.WriteLine();
+
+            if (listaContas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma conta cadastrada"); return;
+            }
+
+            for (int i = 0; i < listaContas.Count; i++)
+            {
+                Conta conta = listaContas[i];
+                Console.Write($"# {i} - ");
+                Console.WriteLine(conta);
+            }
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
+
+            Console.Write("Digite 1 para a conta Física ou 2 para conta Jurídica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o crédito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                        Nome: entradaNome,
+                                        Saldo: entradaSaldo,
+                                        Credito: entradaCredito);
+
+            listaContas.Add(novaConta); // Adiciona uma nova conta na lista de contas.
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bank ao seu dispor");
+            Console.WriteLine("Informa a opção desejada");
+
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar a tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
